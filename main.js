745ea25a00800854b5f51b96d8b38544882481bc
Alessandro-jpg <149205086+Alessandro-jpg@users.noreply.github.com>
2023-11-06 00:43:20 +0100

    Add files via upload
    third project

---
+++ main.js
@@ -0,0 +1,24 @@
+let menu = document.querySelector('.menu_nav')
+let hamburguesa = document.querySelector('.hamburguesa')
+let cerrar = document.querySelector('.cerrar')
+console.log(menu)
+console.log(hamburguesa)
+hamburguesa.addEventListener('click', ()=>{
+    menu.classList.toggle('out')
+})
+cerrar.addEventListener('click', ()=>{
+    menu.classList.remove('out')
+})
+hamburguesa.addEventListener('click', ()=>{
+    
+
+})
+
+
+
+
+        
+        
+
+    
+    
